e85fdde06c673eae05350227397c5c4f8147d533
NomarhSakkulat <seva.corobko@yandex.ru>
2025-04-17 19:01:31 +0400

    Update hello_world.cpp
    Code formatting

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <string>
-
 int main() {
-    std::string name; // храним имя пользователя
-    std::cout << "NomarhSakkulat ";
-    std::cin >> name;
-    std::cout << "Hello world from " << name << std::endl;
+    std::string     name;    // храним имя пользователя
+    std::cout   <<   "NomarhSakkulat";
+    std::cin    >>   name;
+    std::cout   <<   "Hello world"   <<   name   <<   std::endl;
     return 0;
 }
