35e9eb110a77558ca229f05b3b22a4b548ccd361
NomarhSakkulat <seva.corobko@yandex.ru>
2025-04-17 19:38:29 +0400

    Update hello_world.cpp
    conflict resolution

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <string>
+
 int main() {
-    std::string     name;    // храним имя пользователя
-    std::cout   <<   "NomarhSakkulat";
-    std::cin    >>   name;
-    std::cout   <<   "Hello world"   <<   name   <<   std::endl;
+    std::string name; // храним имя пользователя
+    std::cout << "NomarhSakkulat ";
+    std::cin >> name;
+    std::cout << "Hello world from " << name << std::endl;
     return 0;
 }
