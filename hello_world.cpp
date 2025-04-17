f2d148e7773bbbde5041a6c9a4cfe3a38d833ea9
NomarhSakkulat <seva.corobko@yandex.ru>
2025-04-17 19:35:55 +0400

    Revert "Update hello_world.cpp"
    This reverts commit 50caefc60b3c629c601dd782c5adf8f7b520ff56.

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
-#include <string>
+using namespace std;
 
 int main() {
+<<<<<<< HEAD
     std::string name; // храним имя пользователя
     std::cout << "NomarhSakkulat ";
     std::cin >> name;
     std::cout << "Hello world from " << name << std::endl;
+=======
+    string name;
+    cout << "NomarhSakkulat ";
+    cin >> name;
+    cout << "Hello world from " << name << endl;
+>>>>>>> parent of ee11708 (Patch hello_world.cpp)
     return 0;
 }
