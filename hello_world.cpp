50caefc60b3c629c601dd782c5adf8f7b520ff56
NomarhSakkulat <seva.corobko@yandex.ru>
2025-04-17 19:33:16 +0400

    Update hello_world.cpp
    Patch hello_world.cpp
    conflict resolution

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,17 +1,10 @@
 #include <iostream>
-using namespace std;
+#include <string>
 
 int main() {
-<<<<<<< HEAD
     std::string name; // храним имя пользователя
     std::cout << "NomarhSakkulat ";
     std::cin >> name;
     std::cout << "Hello world from " << name << std::endl;
-=======
-    string name;
-    cout << "NomarhSakkulat ";
-    cin >> name;
-    cout << "Hello world from " << name << endl;
->>>>>>> parent of ee11708 (Patch hello_world.cpp)
     return 0;
 }
