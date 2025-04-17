8f97c29e8459238a2b87e29fa6a6bccc78d20222
NomarhSakkulat <seva.corobko@yandex.ru>
2025-04-17 18:36:45 +0400

    Update hello_world.cpp
    Added comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 int main() {
-    std::string name;
+    std::string name; // храним имя пользователя
     std::cout << "NomarhSakkulat ";
     std::cin >> name;
     std::cout << "Hello world from " << name << std::endl;
